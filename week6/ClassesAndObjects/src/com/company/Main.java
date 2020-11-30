6398f5eab0a765cc0e3b734c6cdf4c716ee6c7f1
Szabó Ákos <szabo.akos92@gmail.com>
2020-11-30 21:57:17 +0100

    Blogposts created

--- week6/ClassesAndObjects/src/com/company/Main.java
+++ week6/ClassesAndObjects/src/com/company/Main.java
@@ -19,6 +19,26 @@ public class Main {
         yellow.backgroundColor = "yellow";
         yellow.text = "Fenséges";
 
+        BlogPost loremIpsum = new BlogPost();
+        loremIpsum.title = "Lorem Ipsum";
+        loremIpsum.authorName = "John Doe";
+        loremIpsum.publicationDate = "2010.10.10.";
+        loremIpsum.text = "2010.10.10.";
+
+        BlogPost waitButWhy = new BlogPost();
+        waitButWhy.title = "Wati but why";
+        waitButWhy.authorName = "Tim Urban";
+        waitButWhy.publicationDate = "2010.10.10.";
+        waitButWhy.text = "A popular long-form, stick-figure-illustrated blog about almost everything.";
+
+        BlogPost oneEngeneer = new BlogPost();
+        oneEngeneer.title = "One Engineer Is Trying to Get IBM to Reckon With Trump";
+        oneEngeneer.authorName = "William Turton";
+        oneEngeneer.publicationDate = "2017.03.28.";
+        oneEngeneer.text = "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.";
+
+
+
 
 
 
