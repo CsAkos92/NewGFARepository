71ed66fc871311e6996c3e181eb646132148ff87
Szabó Ákos <szabo.akos92@gmail.com>
2020-11-30 21:26:25 +0100

    Project was initialized.

---
+++ week6/ClassesAndObjects/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PostIt orange = new PostIt();
+        PostIt pink = new PostIt();
+        PostIt yellow = new PostIt();
+
+        orange.textColor = "blue";
+        orange.backgroundColor = "orange";
+        orange.text = "Ötlet 1";
+
+        pink.textColor = "black";
+        pink.backgroundColor = "pink";
+        pink.text = "Csodálatos";
+
+        yellow.textColor = "green";
+        yellow.backgroundColor = "yellow";
+        yellow.text = "Fenséges";
+
+
+
+
+    }
+}
---
+++ week6/ClassesAndObjects/src/com/company/PostIt.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class PostIt {
+
+    //        Készíts egy PostIt osztályt, amelynek van
+    //        backgroundColor (háttérszíne), amelyet egy String reprezentál
+    //        text (szövege), amit ráírtak
+    //        textColor (szöveg színe), amit szintén egy String reprezenát
+    //        Készíts néhány minta post-it példányt:
+    //        egy narancssárgát kék szöveggel: "Ötlet 1"
+    //        egy rózsaszínt fekete szöveggel: "Csodálatos"
+    //        egy citromsárgát zöld szöveggel: "Fenséges!"
+
+    String backgroundColor;
+    String text;
+    String textColor;
+
+}
